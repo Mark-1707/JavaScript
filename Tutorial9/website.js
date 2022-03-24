08dd5cfbe1a18b7904d5f62c1cd1aa2e967dd522
Omkar Shirote <shiroteomkar6@gmail.com>
2022-03-24 22:14:41 +0530

    Manipulation website using js basics

---
+++ Tutorial9/website.js
@@ -0,0 +1,25 @@
+console.log('Manipulating the DOM');
+
+let a = window.document
+
+//window is a global object in javascript
+// alert('Hello Omkar')
+
+// //Assign value using prompt
+// a = prompt('I will destroy your computer')
+
+// //Assign value using confirm(true/false)
+// a = confirm('Are you sure you?')
+
+a = window.innerHeight
+a = window.innerWidth
+a = scrollX
+a = scrollY
+a = location.toString()
+
+//go back
+//history.go(-1)
+
+//location.href = 'https://www.google.com'
+
+console.log(a)
