4c45cb968d6035e7f035ab2a7bb12404ab50e6f1
Omkar Shirote <shiroteomkar6@gmail.com>
2022-03-24 22:32:33 +0530

    Understanding DOM

---
+++ Tutorial10/website.js
@@ -0,0 +1,17 @@
+console.log('Understanding DOM and Creating Website Layout')
+
+let a = document
+
+a = document.all
+
+a = document.body
+
+a = document.forms
+
+// Array.from(a).forEach(function(element){
+//     console.log(element)
+// })
+
+a = document.links
+
+console.log(a)
