f4c59b0f0423491cf594284288a4559f55e31545
Omkar Shirote <shiroteomkar6@gmail.com>
2022-04-02 15:33:33 +0530

    Math Object

---
+++ Tutorial18/script.js
@@ -0,0 +1,25 @@
+console.log('Math object in Javascript');
+
+let x = 3
+let y = 6
+let z
+z = x+y
+console.log(z)
+
+z = Math
+z = Math.PI
+z = Math.E
+z = Math.round(5.6)
+z = Math.ceil(5.4)
+z = Math.floor(5.4)
+z = Math.abs(-2.3)
+z = Math.sqrt(64)
+z = Math.pow(8,2)
+z = Math.min(2,33,4,1,55,6,3,-2)
+z = Math.max(2,33,4,1,55,6,3,-2)
+
+z = Math.random()
+z = Math.random() * 100
+z = Math.random() * (100 - 50) + 50
+
+console.log(z)
