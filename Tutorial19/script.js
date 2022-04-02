744e879afa96208fc0b3649a6f2abb78d0f09976
Omkar Shirote <shiroteomkar6@gmail.com>
2022-04-02 15:47:06 +0530

    Date and Time

--- Tutorial19/script.js
+++ Tutorial19/script.js
@@ -1,1 +1,19 @@
 console.log('Date and Time in Javascript')
+
+let today = new Date()
+//console.log(today)
+
+let otherDate = new Date('June 18, 2019')
+otherDate = new Date('10/18/2019 12:00:00')
+
+a = otherDate.getDay()
+a = otherDate.getDate()
+a = otherDate.getFullYear()
+a = otherDate.getMonth()
+a = otherDate.getHours()
+a = otherDate.getMinutes()
+a = otherDate.getSeconds()
+a = otherDate.getMilliseconds()
+a = otherDate.getTime()
+
+console.log(a)
